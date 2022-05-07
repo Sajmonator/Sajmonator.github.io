ed52ab47ea0a787b288e56fa676bb4ce68508d86
Sajmonator <simon@tripe.cz>
2022-05-07 16:43:23 +0200

    Add files via upload

---
+++ blbost/app.js
@@ -0,0 +1,26 @@
+var output = document.getElementById('moai');
+var btn = document.getElementById('bttn');
+let firstName = 'KKK'; let firstPassword = 'KKK'; //user name, pass
+let AdminPass = "lll"; let AdminName = "lll";//admin name, pass
+
+
+btn.addEventListener('click', function() {
+    var userInputName = document.getElementById('UsLogIn').value;
+    var userInputPass = document.getElementById('UsPass').value;
+    if(!userInputName || !userInputPass){
+        output.innerHTML = `Missing Name or Password.`
+        return;
+    }
+    if((userInputName != (firstName || AdminName)) && (userInputPass != (firstPassword || AdminPass))){
+        output.innerHTML = `Wrong Name or Password`
+        return;
+    }
+    if((userInputName == firstName) && (userInputPass == firstPassword)){
+        output.innerHTML =`cs usere`
+        return;
+    }
+    if((userInputName == AdminName) && (userInputPass == AdminPass)){
+        output.innerHTML =`welcome back admin`
+        return;
+    }
+});
