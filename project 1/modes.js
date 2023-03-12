667cb9cc8eaa355039a60e3453491fb9ec25fff0
Sajmonator <simon@tripe.cz>
2023-03-12 14:16:03 +0100

    some cleaning

--- project 1/modes.js
+++
@@ -1,8 +0,0 @@
-const mod = document.querySelector('.btn');
-const stylus = document.querySelector('.stl');
-mod.addEventListener('click', () =>{
-    if(stylus.getAttribute('href') == 'stylesM.css')
-    {
-        stylus.setAttribute('href', 'darkM.css')
-    } else(stylus.setAttribute('href', 'stylesM.css'))
-})
